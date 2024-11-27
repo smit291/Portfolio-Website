239e7df8b2eab05180f18ca33592451fbb5ff3b4
smit291 <71925859+smit291@users.noreply.github.com>
2024-11-27 11:01:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+let counter = {
+  value: 0,
+};
+
+function updateLoaderText() {
+  let progress = Math.round(counter.value);
+  $(".percentage").text(progress);
+}
+
+let preloaderTl = gsap.timeline({});
+preloaderTl.to(counter, {
+  onUpdate: updateLoaderText,
+  value: 100,
+  duration: 5,
+});
